118cbfa0db0503af4b57c251128e31e19eb2f5ef
Aj <btchaggler@gmail.com>
2014-07-24 18:55:12 +0000

    evolves the string baring one character. maximum fitness achieved 1. desired 0

--- Main.hs
+++ Main.hs
@@ -19,6 +19,7 @@ bitsPerWord = bitsPerLetter * lettersInWord
 
 targetWord = encodeWord "hello"
 
+
 encodeWord :: String -> Word
 encodeWord = concatMap (encodeBinary rangeOfLetters . ord)
 
@@ -31,30 +32,44 @@ wordFitness = flip hammingDistance targetWord
 randomWord :: Rand Word
 randomWord = do
     rndstring <- do s <- randomSample lettersInWord "abcdefghijklmnopqrstuvwxyz"
-        shuffle s
+                    shuffle s
     return $ encodeWord rndstring
 
 randomWordPopulation :: Int -> Rand [Word]
 randomWordPopulation n = replicateM n randomWord
 
+population = 30
+elitecount = 1
+initialpopulation = randomWordPopulation population
+selection = tournamentSelect Minimizing 2 (population - elitecount)
+crossover = onePointCrossover 0.7
+mutation = pointMutate 0.03
+nextgen = nextGeneration Minimizing wordFitness selection elitecount crossover mutation
+stop = (IfObjective ((==0) . minimum))
 
+ioact = DoEvery 1 helper where
+    helper _ p = do
+        let prstr = map (\ (g, f) -> decodeWord g ++ " " ++ (show f)) p
+        mapM_ putStrLn prstr
+
+main = runIO initialpopulation (loopIO [ioact] stop nextgen) >> return ()
 
 --Checking out the library
-fitnessTest = do
-    mt <- newPureMT
-    let pop = replicateM 10 randomWord
-        bits = evalRandom pop mt
-    mapM_ putStrLn $ map (\b -> show (decodeWord b, wordFitness b))bits
+{-fitnessTest = do                                                       -}
+{-    mt <- newPureMT                                                    -}
+{-    let pop = replicateM 10 randomWord                                 -}
+{-        bits = evalRandom pop mt                                       -}
+{-    mapM_ putStrLn $ map (\b -> show (decodeWord b, wordFitness b))bits-}
 
-inbuiltInitialize = do
-    mt <- newPureMT
-    let pop = getRandomBinaryGenomes 10 bitsPerWord
-        bits = evalRandom pop mt
-    mapM_ putStrLn $ map decodeWord bits
+{-inbuiltInitialize = do                                                 -}
+{-    mt <- newPureMT                                                    -}
+{-    let pop = getRandomBinaryGenomes 10 bitsPerWord                    -}
+{-        bits = evalRandom pop mt                                       -}
+{-    mapM_ putStrLn $ map decodeWord bits                               -}
 
-manualInitialize = do
-    mt <- newPureMT
-    let pop = replicateM 10 randomWord
-        bits = evalRandom pop mt
-    mapM_ putStrLn $ map decodeWord bits
+{-manualInitialize = do                                                  -}
+{-    mt <- newPureMT                                                    -}
+{-    let pop = replicateM 10 randomWord                                 -}
+{-        bits = evalRandom pop mt                                       -}
+{-    mapM_ putStrLn $ map decodeWord bits                               -}
 

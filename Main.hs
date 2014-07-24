004bd3999fe6fde31df044743317ab691e42b9a5
Aj <btchaggler@gmail.com>
2014-07-24 17:25:04 +0000

    added functions to encode and decode a word

--- Main.hs
+++ Main.hs
@@ -16,31 +16,45 @@ lettersInWord = 5
 rangeOfLetters = (ord 'a', ord 'z')
 bitsPerLetter = bitsNeeded rangeOfLetters
 bitsPerWord = bitsPerLetter * lettersInWord
-targetWord = concatMap (encodeBinary rangeOfLetters . ord) "hello"
+
+targetWord = encodeWord "hello"
+
+encodeWord :: String -> Word
+encodeWord = concatMap (encodeBinary rangeOfLetters . ord)
+
+decodeWord :: Word -> String
+decodeWord = map (chr. decodeBinary rangeOfLetters) . splitEvery bitsPerLetter
 
 wordFitness :: Word -> Wordscore
-wordFitness w = hammingDistance w targetWord
+wordFitness = flip hammingDistance targetWord
 
 randomWord :: Rand Word
 randomWord = do
-    rndstring <- do
-        s <- randomSample lettersInWord "abcdefghijklmnopqrstuvwxyz"
+    rndstring <- do s <- randomSample lettersInWord "abcdefghijklmnopqrstuvwxyz"
         shuffle s
-    return $ concatMap ((encodeBinary rangeOfLetters) . ord) rndstring
+    return $ encodeWord rndstring
 
 randomWordPopulation :: Int -> Rand [Word]
 randomWordPopulation n = replicateM n randomWord
 
+
+
 --Checking out the library
+fitnessTest = do
+    mt <- newPureMT
+    let pop = replicateM 10 randomWord
+        bits = evalRandom pop mt
+    mapM_ putStrLn $ map (\b -> show (decodeWord b, wordFitness b))bits
+
 inbuiltInitialize = do
     mt <- newPureMT
     let pop = getRandomBinaryGenomes 10 bitsPerWord
         bits = evalRandom pop mt
-    mapM_ putStrLn $ map (map (chr. decodeBinary rangeOfLetters) . splitEvery bitsPerLetter) bits
+    mapM_ putStrLn $ map decodeWord bits
 
 manualInitialize = do
     mt <- newPureMT
     let pop = replicateM 10 randomWord
         bits = evalRandom pop mt
-    mapM_ putStrLn $ map (map (chr. decodeBinary rangeOfLetters) . splitEvery bitsPerLetter) bits
+    mapM_ putStrLn $ map decodeWord bits
 
